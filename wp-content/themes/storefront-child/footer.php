9b7be98500cf3c2740189ac942f2a4915bf62a0d
Hayden Worden <haydenworden@outlook.com>
2018-12-06 13:28:58 +0000

    Home icon for handheld navbar

--- wp-content/themes/storefront-child/footer.php
+++ wp-content/themes/storefront-child/footer.php
@@ -1,23 +1,28 @@
-<?php wp_footer(); ?>
 <?php do_action( 'storefront_before_footer' ); ?>
-  <section class='my-container'>
-    <footer id="colophon" class="site-footer" role="contentinfo">
-    <?php
-      wp_nav_menu(array(
-        'theme_location'=>'main_footer',
-      ));
-      ?>
+
+<footer id="colophon" class="site-footer" role="contentinfo">
+  <div class="col-full">
+      <div class="">
+          <?php
+            wp_nav_menu(array(
+              'theme_location'=>'footer_main',
+            ));
+          ?>
+      </div>
     <?php
     /**
-    * Functions hooked in to storefront_footer action
-    *
-    * @hooked storefront_footer_widgets - 10
-    * @hooked storefront_credit         - 20
-    */
-    ?>
-    </footer>
-  </div> <!--Loading page icon  -->
-  <?php do_action( 'storefront_after_footer' ); ?>
-  </section>
-</body>
+     * Functions hooked in to storefront_footer action
+     *
+     * @hooked storefront_footer_widgets - 10
+     * @hooked storefront_credit         - 20
+     */
+    do_action( 'storefront_footer' ); ?>
+
+  </div><!-- .col-full -->
+</footer><!-- #colophon -->
+</div> <!--Loading page icon  -->
+<?php do_action( 'storefront_after_footer' ); ?>
+<!-- <script type="text/javascript" src=""></script> -->
+<?php wp_footer() ?>
+  </body>
 </html>
