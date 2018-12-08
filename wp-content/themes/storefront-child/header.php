e64c83d78aa8b93d174d7f0ad0f83cc5a6bc10c7
Hayden Worden <haydenworden@outlook.com>
2018-12-08 20:09:25 +0000

    Navbar cart improved

--- wp-content/themes/storefront-child/header.php
+++ wp-content/themes/storefront-child/header.php
@@ -27,7 +27,7 @@
 <?php endif; ?>
   <section id='shop-banner'>
 			<?php if ( get_option('op_banner_image_url') ) : ?>
-				<img src="<?php echo esc_url( get_option('op_banner_image_url') ); ?>">
+				<img id='banner-img' src=' <?php  echo esc_url(   get_option('op_banner_image_url') ); ?>'>
 			<?php endif; ?>
   </section>
 <?php if ( get_option('banner_image_link') && get_option('op_banner_image_url')) : ?>
@@ -36,20 +36,17 @@
 <section class='my-container'>
 	<header id="masthead" class="site-header" role="banner" style="<?php storefront_header_styles(); ?>">
 	<section id="category-bar">
-		<nav class="navbar navbar-expand-lg navbar-dark ">
+		<nav class="navbar navbar-expand-lg navbar-light ">
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
 			<div class="collapse navbar-collapse" id="navbarSupportedContent">
-				<div id="nav-contain">
-					<div class="nav-box">
-						<?php storefront_header_cart() ?>
-					</div>
-					<div>
 						<?php
 							wp_nav_menu(array(
 							'theme_location'=>'category_bar',
 							 'depth' => 10,
+							 'items_wrap' => '<div id="nav-contain"><div class="nav-box">'. storefront_header_cart() .'
+							 </div></div> <ul id="%1$s" class="%2$s">%3$s</ul>'
 						));
 						?>
 					</div>
